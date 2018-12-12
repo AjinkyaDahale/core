2b879e725b8ce7f869f85417937d5fb7e43f858c
Ajinkya Dahale <dahala@rpi.edu>
2018-12-11 22:25:41 -0500

    [ma][elem-rem-coll] Elem-rem-coll code cleanup
    Removed making elements all the time (e.g. when checking concavity).

--- ma/maElemRemCollapse.cc
+++ ma/maElemRemCollapse.cc
@@ -80,6 +80,17 @@ static void showBFaces(Adapt* a, const BFaceMap& bFaceMap, const char* name)
   ma_dbg::createCavityMesh(a, faces, name, apf::Mesh::TRIANGLE);
 }
 
+static bool isTetVolPositive(Mesh* m, Entity** verts)
+{
+  Vector v[4];
+  for (size_t i = 0; i < 4; ++i) {
+    m->getPoint(verts[i], 0, v[i]);
+  }
+  if ((cross((v[1] - v[0]), (v[2] - v[0])) * (v[3] - v[0])) < 0)
+    return false;
+  return true;
+}
+
 // static void showNewEnts(Adapt* a, const EntitySet& newEnts, const char* name)
 // {
 //   EntityArray tets;
@@ -96,7 +107,7 @@ static void showBFaces(Adapt* a, const BFaceMap& bFaceMap, const char* name)
 // }
 
 Entity* ElemRemCollapse::buildOrFind(Adapt* a, int type, Entity** vs,
-                           bool* elemMade, bool* elemInverted)
+                                     bool* elemMade, bool* elemInverted)
 {
   Mesh* m = a->mesh;
 
@@ -219,14 +230,6 @@ bool ElemRemCollapse::markEdges(Mesh* m, Entity* face, bool dryRun)
   for (int k = 0; k < 3; k++) {
     if(!getFlag(adapt, edges[k], MARKED)) {
       PCU_ALWAYS_ASSERT(bEdgeMap.count(edges[k]) == 0);
-      // if (bEdgeMap.count(edges[k]) != 0) {
-      //   ma_dbg::createCavityMesh(adapt, newEntsArray, "new_ents_array", apf::Mesh::TET);
-      //   showBFaces(adapt, bFaceMap, "bfaces");
-      //   EntityArray offender;
-      //   offender.append(edges[k]);
-      //   ma_dbg::createCavityMesh(adapt, offender, "offending_edge", apf::Mesh::EDGE);
-      //   apf::fail("Some edge is remaining in bedgemap despite no marked flag!\n");
-      // }
       // Unmarked edges can always be marked
       if (dryRun) continue;
       BEdge be1;
@@ -241,34 +244,35 @@ bool ElemRemCollapse::markEdges(Mesh* m, Entity* face, bool dryRun)
       if (bEdgeMap[edges[k]].face2) {
         if (dryRun) return false;
         else
-          PCU_ALWAYS_ASSERT_VERBOSE(false, "Attempted to mark an edge with more than one faces already defined.\n");}
+          PCU_ALWAYS_ASSERT_VERBOSE(false, "Attempted to mark an edge with more"
+                                    " than one faces already defined.\n");
+      }
       if (dryRun) continue;
       Entity* face1 = bEdgeMap[edges[k]].face1;
       bEdgeMap[edges[k]].face2 = face;
       bEdgeMap[edges[k]].cda = getCosDihedral(adapt, edges[k],
                                               face, bFaceMap[face].first,
                                               face1, bFaceMap[face1].first);
-      // The `!=` acts as XOR. We invert if exactly one of the reference elements is negative
+      // The `!=` acts as XOR. We invert if exactly one of the reference
+      // elements is negative
       if (bFaceMap[face1].second != bFaceMap[face].second)
         bEdgeMap[edges[k]].cda *= -1;
 
+      // A check to see if the dihedral angle is < pi or not
       // TODO: is there a better way to compute angles than making the element
       // If element made is negative, the dihedral angle > pi
-      bool tryEntMade;
-      Entity* tryEnt = removeEdge(edges[k], &tryEntMade);
-      double tryEntQual = -1;
-      if (tryEnt)
-        tryEntQual = adapt->shape->getQuality(tryEnt);
-      if (tryEntQual < adapt->input->validQuality) {
+      Entity* vs[4];
+      orientForBuild(m, getTriVertOppositeEdge(m, face, edges[k]), face1,
+                     bFaceMap[face1].first, bFaceMap[face1].second, vs);
+      if (!isTetVolPositive(m, vs)) {
         bEdgeMap[edges[k]].cda = -2 - bEdgeMap[edges[k]].cda;
-      } else if (tryEntQual < this->qualToBeat &&
-                 bEdgeMap[edges[k]].cda > 0) {
+      } else if (bEdgeMap[edges[k]].cda > 0.9) {
+        // TODO: could we use a less arbitrary condition for too acute angles?
+        // Just reply that the angle is too acute for our liking
         if (ignoredFaceClassifn.empty() ||
             ignoredFaceClassifn.count(m->toModel(edges[k]))==0)
-        areNewAnglesGood = false;
+          areNewAnglesGood = false;
       }
-      if (tryEnt && tryEntMade) destroyElement(adapt, tryEnt);
-      else PCU_ALWAYS_ASSERT((!tryEnt) || m->toModel(tryEnt) != NULL);
     }
   }
 
@@ -321,7 +325,8 @@ bool ElemRemCollapse::setCavity(apf::DynamicArray<Entity*>& elems)
   // "Dry run" to mark all boundary faces
   for (int i = 0; i < numElems; ++i) {
     PCU_ALWAYS_ASSERT_VERBOSE(apf::Mesh::typeDimension[m->getType(elems[i])] == d,
-                              "Desired cavity contains entities of different dimension than that of mesh.\n");
+                              "Desired cavity contains entities of different "
+                              "dimension than that of mesh.\n");
     // We do not want to cross model region boundaries
     if (m->toModel(elems[i]) != modelEnt) continue;
     setFlag(adapt, elems[i], CAV_OLD);
@@ -681,9 +686,6 @@ bool ElemRemCollapse::tryThisDirectionNoCancel(double qualityToBeat)
     return false;
   // since they are okay in a linear sense, now fit and do a quality assessment
   fitElements();
-  // The following check should already be handled in makeNewElements
-  // if (hasWorseQuality(adapt,newElements,qualityToBeat))
-  //   return false;
 
   return true;
 }
--- ma/maElemRemCollapse.h
+++ ma/maElemRemCollapse.h
@@ -29,86 +29,86 @@ public:
 typedef std::map<Entity*, BEdge> BEdgeMap;
 // The values correspond to <reference element, is ref elem positive>
 typedef std::map<Entity*, std::pair<Entity*, bool> > BFaceMap;
-  typedef std::map<Model*, EntitySet> ClassifnGroups;
- class ElemRemCollapse : public Collapse
+typedef std::map<Model*, EntitySet> ClassifnGroups;
+class ElemRemCollapse : public Collapse
 {
-  public:
-    virtual void Init(Adapt* a);
+public:
+  virtual void Init(Adapt* a);
 
-    bool setCavity(apf::DynamicArray<Entity*>& elems);
-    bool addElement(Entity* e, bool isOld = false);
+  bool setCavity(apf::DynamicArray<Entity*>& elems);
+  bool addElement(Entity* e, bool isOld = false);
   void setIgnoredModelFaces(EntityArray& ups);
   void addClassifnGroups(EntityArray& ups);
 
-    /** Only creates an element with the edge and adjacent
-	faces on cavity surface. May return NULL under certain circumstances.*/
-    Entity* removeEdge(Entity* e, bool* elemMade);
-    /** Removes an element determined by the given face on cavity boundary
-        and another cavity boundary face sharing an edge with it. */
-    Entity* removeFace(Entity* e, bool* elemMade);
-    /** Remove the highest dimension entity */
-    bool removeElement(Entity* e);
+  /** Removes an element determined by the given face on cavity boundary
+      and another cavity boundary face sharing an edge with it. */
+  Entity* removeFace(Entity* e, bool* elemMade);
+  /** Only creates an element with the edge and adjacent
+      faces on cavity surface. May return NULL under certain circumstances.*/
+  Entity* removeEdge(Entity* e, bool* elemMade);
+  /** Remove the highest dimension entity */
+  bool removeElement(Entity* e);
 
-    bool makeNewElements(double qualityToBeat);
-    void cancel(bool cavOnly);
-    virtual void cancel();
-    void transfer();
-    virtual void destroyOldElements();
-    virtual void destroyNewElements();
-    virtual bool tryThisDirectionNoCancel(double qualityToBeat);
-    void unmark(bool cavOnly = false);
+  bool makeNewElements(double qualityToBeat);
+  void cancel(bool cavOnly);
+  virtual void cancel();
+  void transfer();
+  virtual void destroyOldElements();
+  virtual void destroyNewElements();
+  virtual bool tryThisDirectionNoCancel(double qualityToBeat);
+  void unmark(bool cavOnly = false);
 
-  private:
-    bool markEdges(Mesh* m, Entity* face, bool dryRun = false);
-    void unmarkEdges(Mesh* m, Entity* face);
+private:
+  bool markEdges(Mesh* m, Entity* face, bool dryRun = false);
+  void unmarkEdges(Mesh* m, Entity* face);
   bool edgeIntersectsFace(Adapt* a, Entity* face, Entity* edge);
 
   Entity* buildOrFind(Adapt* a, int type, Entity** vs,
                       bool* elemMade, bool* elemInverted=NULL);
 
-    ClassifnGroups classifnGroups;
+  ClassifnGroups classifnGroups;
   
-    bool newTetClear(Adapt* a, Entity* tet);
+  bool newTetClear(Adapt* a, Entity* tet);
 
-    double qualToBeat;
-    /* Adapt* adapt; */
-    apf::ModelEntity* modelEnt;
+  double qualToBeat;
+  /* Adapt* adapt; */
+  apf::ModelEntity* modelEnt;
 
-    EntitySet oldEnts;
-    EntitySet newEnts;
-    EntityArray newEntsArray;
-    BEdgeMap bEdgeMap;
-    BFaceMap bFaceMap;
-    std::vector<Entity*> edgesInQueue;
+  EntitySet oldEnts;
+  EntitySet newEnts;
+  EntityArray newEntsArray;
+  BEdgeMap bEdgeMap;
+  BFaceMap bFaceMap;
+  std::vector<Entity*> edgesInQueue;
 
   std::set<Model*> ignoredFaceClassifn;
 
-    class compareEdgeByCosAngle {
-    public:
+  class compareEdgeByCosAngle {
+  public:
     compareEdgeByCosAngle(BEdgeMap& bEdgeMap) : _bEdgeMap(&bEdgeMap) {}
-      bool operator()(Entity* a, Entity* b)
-      {
-        // recall that this returns whether a compares _less_ that b, and
-        // and std::make_heap returns a _max_ heap
-        // if a is not on cavity boundary, pop it out first
-        if(_bEdgeMap->count(a) == 0) return false;
-        // likewise for b
-        if(_bEdgeMap->count(b) == 0) return true;
-        return (*_bEdgeMap)[a].cda > (*_bEdgeMap)[b].cda;
-      }
-    private:
-      BEdgeMap* const _bEdgeMap;
-    };
+    bool operator()(Entity* a, Entity* b)
+    {
+      // recall that this returns whether a compares _less_ that b, and
+      // and std::make_heap returns a _max_ heap
+      // if a is not on cavity boundary, pop it out first
+      if(_bEdgeMap->count(a) == 0) return false;
+      // likewise for b
+      if(_bEdgeMap->count(b) == 0) return true;
+      return (*_bEdgeMap)[a].cda > (*_bEdgeMap)[b].cda;
+    }
+  private:
+    BEdgeMap* const _bEdgeMap;
+  };
 
   class ElementBuilder2 : public apf::ElementVertOp
   {
   public:
     ElementBuilder2(
-        Mesh* m,
-        int baseType,
-        ClassifnGroups& classifnGroups,
-        apf::BuildCallback* cb,
-        bool* elemMade) :
+      Mesh* m,
+      int baseType,
+      ClassifnGroups& classifnGroups,
+      apf::BuildCallback* cb,
+      bool* elemMade) :
       _classifnGroups(classifnGroups)
     {
       mesh = m;
@@ -165,7 +165,7 @@ typedef std::map<Entity*, std::pair<Entity*, bool> > BFaceMap;
     int _baseType;
     apf::BuildCallback* callback;
   };
-  
+
 };
 
 }
